ba9857dd298c5ecf7879139b6072ffc70b34425e
Inara Oenning <inaraoenning@gmail.com>
2025-07-04 16:48:58 -0300

    Adicionando Componente botão conteúdo a página e Btn primary

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Header } from '../header/header';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [Header],
+  imports: [Header, NgOptimizedImage],
   providers: [],
   templateUrl: './home.html',
   styleUrls: ['./home.scss']
